81b6fbe488a3fbfea8ac563da938448a7e44f989
tobias-jenkner <tobias.jenkner@googlemail.com>
2024-05-14 18:46:59 +0200

    script to generate different aspects of flux install

---
+++ generate.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+
+# create flux resource definitions to install or update flux
+# based on the 'flux' executable available from https://github.com/fluxcd/flux2/releases
+
+here=`dirname $(realpath$0)`
+
+flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "CustomResourceDefinition")' > $here/kubeadmin/flux-crds.yml
+# flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "ClusterRole")' > $here/flux-clusterroles.yml
+# flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "ClusterRole") | select(.metadata.name == "flux-edit-flux-system")' > $here/cluster-role-flux-edit-flux-system.yml
+# flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "ClusterRole") | select(.metadata.name == "flux-edit-flux-system" or .metadata.name == "flux-view-flux-system")' > $here/kubeadmin/flux-clusterroles.yml
+
+flux install --watch-all-namespaces=false--export | yq e '. | select(.kind == "ClusterRole")' > $here/kubeadmin/flux-clusterroles.yml
+# flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "ClusterRoleBinding") | .kind="RoleBinding"' > $here/flux-rolebindings.yml
+flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "NetworkPolicy") | del(.metadata.namespace)' > $here/developer/flux-network-policies.yml
+flux install --watch-all-namespaces=false --export | yq e '. | select(.kind == "Deployment" or .kind == "Service" or .kind == "ServiceAccount") | del(.metadata.namespace)' > $here/developer/flux-controllers.yml
